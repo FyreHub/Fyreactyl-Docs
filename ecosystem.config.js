024ba04c20921b09dec7ddd623c0e061bfd4bc4d
Liam L <thefallenspirit@outlook.com>
2022-06-07 09:26:24 -0700

    Create ecosystem.config.js

---
+++ ecosystem.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+    apps: [
+        {
+            name: 'Docs - FyreSoftware',
+            script: 'yarn',
+            args: 'run serve -- --build --port 3000 --host 0.0.0.0'
+        }
+    ]
+}
